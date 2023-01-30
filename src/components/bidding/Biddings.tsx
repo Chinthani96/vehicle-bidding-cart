11086b89c0afa93bfcbe2d53b1524622ec7d0790
Chinthani <chinthani96@gmail.com>
2023-01-30 18:16:10 +0530

    Add bidding item layout :fire:

--- src/components/bidding/Biddings.tsx
+++ src/components/bidding/Biddings.tsx
@@ -9,6 +9,9 @@ const Biddings = () => {
       </div>
       <hr />
       <div className="relative bottom-0 left-0">Total: $2000000</div>
+      <button className="rounded-md bg-bg text-header-color my-2 w-20 h-[2rem]">
+        Close
+      </button>
     </Modal>
   );
 };
