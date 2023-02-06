97a49a2260aa40e7433687d02afa87bd2d469bdd
Chinthani <chinthani96@gmail.com>
2023-02-06 11:54:11 +0530

    Configure provider in main.tsx

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,4 +1,5 @@
-const { configureStore } = require("@reduxjs/toolkit");
+import { configureStore } from "@reduxjs/toolkit";
+import vehicleSlice from "./vehicle-slice";
 
 const store = configureStore({
     reducer: {
