09d2bbad8c66bdd673b654db918173c774d5601b
Chinthani <chinthani96@gmail.com>
2023-02-06 14:08:20 +0530

    Add brand filtering functionality

--- src/components/vehicle/BrandFilter.tsx
+++ src/components/vehicle/BrandFilter.tsx
@@ -1,10 +1,11 @@
-import { useSelector } from "react-redux";
-import { vehicleSelector } from "../../store/vehicle-slice";
-import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { setBrand, vehicleSelector } from "../../store/vehicle-slice";
+import { ChangeEvent, useEffect, useState } from "react";
 
 const BrandFilter = () => {
   const { vehicles } = useSelector(vehicleSelector) || [];
   const [brands, setBrands] = useState<string[]>([]);
+  const dispatch = useDispatch();
 
   useEffect(() => {
     getBrands();
@@ -20,10 +21,28 @@ const BrandFilter = () => {
     setBrands(unique);
   };
 
+  const handleOnChange = (e: ChangeEvent<HTMLSelectElement>) => {
+    if (e.target.value == "None") {
+      dispatch(setBrand(""));
+    } else {
+      dispatch(setBrand(e.target.value));
+    }
+  };
+
   return (
-    <select name="cars" id="cars" className="w-1/4 h-7/6 bg-bg rounded-md px-4">
-      {brands && brands.map((brand) => <option>{brand}</option>)}
-      {/* <option value="volvo">Volvo</option> */}
+    <select
+      name="cars"
+      id="cars"
+      className="w-1/4 h-7/6 bg-bg rounded-md px-4"
+      onChange={handleOnChange}
+    >
+      {brands &&
+        brands.map((brand) => (
+          <option key={brands.indexOf(brand)} value={brand}>
+            {brand}
+          </option>
+        ))}
+      <option defaultValue="">None</option>
     </select>
   );
 };
--- src/components/vehicle/VehicleCard.tsx
+++ src/components/vehicle/VehicleCard.tsx
@@ -4,15 +4,15 @@ import BiddingForm from "../bidding/BiddingForm";
 const VehicleCard = (props: Vehicle) => {
   const { name, details, id } = props;
   return (
-    <div className="w-100 h-[30rem] rounded-md shadow-2xl p-5 text-text-color">
+    <div className="w-100 h-[35rem] rounded-md shadow-2xl p-5 text-text-color">
       <h3>{name}</h3>
       <hr className="my-2" />
       <img src={details.image} alt="car" className="min-h-[15rem]" />
-      <p>
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Odio
-        assumenda, nemo nam suscipit ullam neque sit laudantium provident, culpa
-        officiis repellendus obcaecati quam fuga in reiciendis fugit possimus
-        nesciunt molestias.
+      <p className="min-h-[8rem]">
+        <b>{details.description}</b>
+        <br />
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis,
+        repellat! Suscipit eligendi...
       </p>
       <BiddingForm />
     </div>
