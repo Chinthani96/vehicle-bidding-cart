4aba74d614c1d9171cd88c6b456ce84d32e0391b
Chinthani <chinthani96@gmail.com>
2023-02-06 12:00:36 +0530

    Add brandfilter component to the layout

--- src/components/header/VBHeader.tsx
+++ src/components/header/VBHeader.tsx
@@ -1,9 +1,11 @@
+import BrandFilter from "../vehicle/BrandFilter";
 import CartButton from "./CartButton";
 
 const VBHeader = () => {
   return (
     <header className=" bg-header-color w-full h-20 flex py-5 px-10 justify-between">
       <h2 className="text-2xl font-bold text-bg">Bidding Cart</h2>
+      <BrandFilter />
       <CartButton />
     </header>
   );
